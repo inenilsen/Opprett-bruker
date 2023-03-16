48df7f67ba152c2b579d68ef56834f7d9941c20b
Ine <ine.elene.nilsen@gmail.com>
2023-03-16 13:40:16 +0100

    Sorting files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import Info from './Info'
+import Opprett from './Opprett'
+
+export default function App() {
+    return (
+        <div class="container">
+            <Info/>
+            <Opprett/>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import Info from './Info'
-import Opprett from './Opprett'
+import App from './App'
 
-function Side() {
-    return (
-        <div class="container">
-            <Info/>
-            <Opprett/>
-        </div>
-    )
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<Side/>)
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
